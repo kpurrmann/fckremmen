eea7866116a48714b1c7735b42994b986f25bc24
kevin <kevin@otto>
2012-07-03 14:11:03 +0200

    First

---
+++ fileadmin/fck.ts
@@ -0,0 +1,379 @@
+lib.t3sports.profile.tntrikot =< lib.t3sports.teamnote
+
+[globalVar = TSFE:id=33, TSFE:page|pid=33]
+lib.t3sports.profile {
+  firstpicture =< lib.t3sports.profile.pictures
+  firstpicture {
+    template = EXT:cfc_league_fe/res/lightboxpics.html  
+    limit = 1
+    offset = 0
+    subpartName = ###FIRSTPICTURE###
+	media {
+      file = IMG_RESOURCE
+      file.file.import.field = file
+      file.file.maxW = {$plugin.tx_cfcleaguefe_report.maxW_profilePicture}
+      file.file.maxH = {$plugin.tx_cfcleaguefe_report.maxH_profilePicture}
+      thumbnail < .file
+      thumbnail = IMAGE
+      thumbnail.file.maxW = 200
+      thumbnail.file.maxH = 300
+      thumbnail.params = border="0"
+      thumbnail.titleText.field = title
+    }
+  }
+}
+
+[global]
+#| --- CODE CLEANUP ------------------------------------------------------------------------------------ |#
+plugin.tx_cssstyledcontent._CSS_DEFAULT_STYLE >
+
+tt_content {
+	stdWrap.dataWrap >
+	stdWrap.prefixComment >
+	header.20.prefixComment >
+	default.prefixComment >
+	text.stdWrap.prefixComment >
+	text.20.prefixComment >
+	textpic.20.stdWrap.prefixComment >
+	table.20.stdWrap.prefixComment >
+	menu.20.stdWrap.prefixComment >
+	image.20.stdWrap.prefixComment >
+	list.20.stdWrap.prefixComment >
+}
+
+// Kill spaces and clear.gif
+lib.stdheader.stdWrap.space = 0|0
+lib.stdheader.stdWrap.dataWrap >
+tt_content.stdWrap >
+tt_content {
+	stdWrap.space = 0|0
+	stdWrap.spaceBefore = 0
+	stdWrap.spaceAfter = 0
+	header.stdWrap.space = 0|0
+	textpic.20 {
+		spaceBelowAbove = 0
+		noStretchAndMarginCells = 1
+		1.params =
+	}
+	
+}
+
+
+
+#| --- Page Config ------------------------------------------------------------------------------------ |#
+config {
+	sys_language_uid = 0
+	language = de
+	locale_all = de_DE 
+}
+
+page = PAGE
+page.config {
+	doctype = xhtml_trans
+	xhtmlDoctype = xhtml_trans
+	xhtml_cleaning = all
+	disableImgBorderAttr = 1
+	disablePrefixComment = 1
+	htmlTag_langKey = de-DE 
+	pageTitleFirst = 1
+	removeDefaultJS = 1
+	inlineStyle2TempFile = 1
+	simulateStaticDocuments = 1
+	simulateStaticDocuments_noTypeIfNoTitle = 1
+	metaCharset = utf-8
+	additionalHeaders = Content-Type:text/html;charset=utf-8	
+	no_cache =1
+	nocache =1
+}
+	
+
+
+#| --- Header & Body ---------------------------------------------------------------------------------- |#
+page.meta { 
+	robots = FOLLOW, INDEX
+	author = Kevin Purrmann
+	MSSmartTagsPreventParsing = true
+	imagetoolbar = false
+	language = de
+}
+
+page.typeNum = 0
+page.bodyTag = <body>
+page.stylesheet = fileadmin/css/web.css
+page.headerData.3 = TEXT
+page.headerData.3.value ( 
+	<link rel="stylesheet" type="text/css" href="fileadmin/src/index.css" />
+	<script language="javascript" type="text/javascript" src="fileadmin/src/jquery-1.3.2.min.js"></script>
+	<script language="javascript" type="text/javascript" src="fileadmin/src/jquery.lightbox-0.5.js"></script>
+	<script language="javascript" type="text/javascript" src="fileadmin/src/jquery.sorter.js"></script>	
+	<script language="javascript" type="text/javascript" src="fileadmin/src/animationen.js"></script>
+	<script type="text/javascript" src="http://static.fussball.de/fbdeAPI/js/fbdeAPIFunctions.js?schluessel=15829EFE3AE49E9CD2B4142480261EAB6E730DA9"></script>
+)
+
+#|-----------tt_content------------------#
+
+lib.stdheader.10.2.fontTag = <div class="boxhead"><h2>|</h2></div>
+
+temp.SPONSOREN < plugin.tx_macinabanners_pi1
+temp.SPONSOREN {
+	results_at_a_time = 3
+	placement = top
+	mode = random_all
+	pidList = 29
+	recursive = 0
+	results_at_a_time = 3
+}
+
+temp.ARTIKEL < temp.SPONSOREN
+temp.ARTIKEL {
+	placement >
+	placement = left
+	pidList >
+	pidList = 40
+	results_at_a_time = 3
+}
+
+plugin.tt_news {
+	templateFile = fileadmin/news_templates.html
+	code = LATEST
+	pid_list = 34
+	singlePid = 35
+	limit = 9
+	categoryMode = 0
+	latestLimit = 3
+	displayLatest {
+		image {
+		    file.maxW = 705
+		    file.maxH = 290
+		    imageLinkWrap = 1
+		    stdWrap.spaceAfter >
+		}
+	}
+}
+
+[globalVar = TSFE:id=15, TSFE:page|pid=15]
+plugin.tt_news {
+	categoryMode = 2
+	categorySelection = 2
+}
+[global]
+
+lib.newsList < plugin.tt_news
+lib.newsList {
+	code = LIST2
+	excludeAlreadyDisplayedNews = 1
+	displayList {
+		image {
+			file.maxW = 100
+			file.maxH = 66
+			imageLinkWrap = 1
+			stdWrap.spaceAfter >
+		}
+		subheader_stdWrap {
+		  crop = 300 | ... | 1
+		  ifEmpty.field = bodytext
+		}
+	}
+}
+
+
+# MENU
+
+tt_content.menu = COA
+tt_content.menu {
+	20 {
+
+		1 {
+			wrap = <ul class="menu">|</ul>
+			1.NO.noBlur = 1
+			1.CUR = 1
+			1.CUR.doNotLinkIt = 1
+		}
+		4 {
+			wrap >
+			1 = GMENU
+			1 {
+				wrap = <ul>|</ul>		 
+				noBlur = 1
+				NO = 1
+				NO {
+					wrap = <li>|</li> 
+					altImgResource.import = uploads/media/
+					altImgResource.import.field = media
+					altImgResource.import.listNum = 0
+					ATagTitle.field = subtitle // title
+				}
+			}
+		}
+	}
+}
+
+#| --- Template --------------------------------------------------------------------------------------- |#
+
+page.10 = TEMPLATE
+page.10 {
+	template = FILE
+	workOnSubpart = DOC
+	template.file = fileadmin/index.html	
+}
+
+[globalVar=TSFE:page|layout=1] 
+	page.10.template.file = fileadmin/news.html 
+[global]
+
+[globalVar=TSFE:page|layout=2] 
+	page.10.template.file = fileadmin/mannschaften.html 
+[global] 
+
+
+#| --- Markers & Subparts ----------------------------------------------------------------------------- |#
+
+page.10 {
+
+	subparts.NEWS < plugin.tt_news
+	
+	subparts.CONTENT = CONTENT
+	subparts.CONTENT < styles.content.get
+	
+	subparts.CONTENT_RIGHT = CONTENT
+	subparts.CONTENT_RIGHT {
+		table = tt_content
+		select {
+			where = colPos = 2
+			begin = 0
+			max = 4
+		}
+		renderObj = COA
+		renderObj.wrap = <div class="box"><div class="boxbody">|</div></div>
+		renderObj.10 < tt_content
+	}
+		
+	subparts.TEASER = CONTENT
+	subparts.TEASER = styles.content.getLeft
+	
+	subparts.CONTENT_LEFT = CONTENT		
+	subparts.CONTENT_LEFT < styles.content.getLeft
+
+	subparts.FOOTER = CONTENT
+	subparts.FOOTER {
+		table=tt_content
+		select {
+				pidInList = 9 #Seite ist im Pool
+				where = colPos = 1
+	            begin = 0
+	            max = 1
+        }
+	}
+		
+	subparts.MAINMENU = HMENU
+	subparts.MAINMENU {
+		1 = TMENU
+		1 {
+			wrap >
+			expAll = 0
+			noBlur = 1
+			NO.ATagParams = class="main"						
+			NO.ATagTitle.field = header // title // subtitle // alias			
+			NO.wrapItemAndSub = <li>|</li>
+		}
+	}
+	
+	subparts.SUBMENU_START = HMENU
+	subparts.SUBMENU_START {
+		special = directory
+		special.value = 2
+		1 = TMENU
+		1 {
+			wrap >
+			expAll = 0
+			noBlur = 1
+			ATagParams = 1
+			NO.ATagTitle.field = header // title // subtitle // alias			
+			NO.wrapItemAndSub = <li>|</li>
+		}
+	}	
+
+	subparts.SUBMENU_VEREIN = HMENU
+	subparts.SUBMENU_VEREIN {
+		special = directory
+		special.value = 7
+		1 = TMENU
+		1 {
+			wrap >
+			expAll = 0
+			noBlur = 1
+			ATagParams = 1
+			NO.ATagTitle.field = header // title // subtitle // alias			
+			NO.wrapItemAndSub = <li>|</li>
+		}
+	}		
+	
+	subparts.SUBMENU_MANNSCHAFT = HMENU
+	subparts.SUBMENU_MANNSCHAFT {
+		special = directory
+		special.value = 8
+		1 = TMENU
+		1 {
+			wrap >
+			expAll = 0
+			noBlur = 1
+			ATagParams = 1
+			NO.ATagTitle.field = header // title // subtitle // alias			
+			NO.wrapItemAndSub = <li>|</li>
+		}
+	}	
+
+	subparts.SUBMENU_JUNIOREN = HMENU
+	subparts.SUBMENU_JUNIOREN {
+		special = directory
+		special.value = 9
+		1 = TMENU
+		1 {
+			wrap >
+			expAll = 0
+			noBlur = 1
+			ATagParams = 1
+			NO.ATagTitle.field = header // title // subtitle // alias			
+			NO.wrapItemAndSub = <li>|</li>
+		}
+	}	
+	
+	subparts.SUBMENU = HMENU
+	subparts.SUBMENU {
+		entryLevel = 1
+		1 = TMENU
+		1 {
+			wrap = |
+			expAll = 1
+			NO.wrapItemAndSub = ||&nbsp;&#124&nbsp;||&nbsp;&#124&nbsp;|*|
+		}
+	}
+	
+	subparts.SPONSOREN >
+	subparts.ARTIKEL < temp.SPONSOREN
+	subparts.FURTHER_NEWS < lib.newsList
+	
+	marks.LIGHTBOX = TEXT
+	marks.LIGHTBOX.value = 
+
+}
+[globalVar = TSFE:id=2, TSFE:page|pid=2]
+page.10 {
+	marks.LIGHTBOX = COA
+	marks.LIGHTBOX {
+		10 = CONTENT
+		10 {
+			table = tt_content
+			select {
+				 pidInList = 2
+				 where = colPos = 3
+				 max = 1
+			}
+			wrap = <div id="newsflash" style="display:none;">|</div>
+			required = 1
+		}
+
+		}
+	}	
+}
+[global]
---
+++ fileadmin/src/animationen.js
@@ -0,0 +1,131 @@
+// Animationen für Goldrausch
+
+$(document).ready(function() {
+
+	/* MENUFÜHRUNG */
+	var inter = 0;
+		
+	$('#menu li a.main').mouseover(function() {
+		var i = $('#menu li a').index(this);		
+		$('div.aktiv').removeClass('aktiv');
+		$('#submenu:hidden').slideDown();
+		$('div.col-' + (i+1)).addClass('aktiv');		
+		$('#menu img').removeClass('einfahren');
+		$('#menu img').addClass('ausfahren');			
+		clearInterval(inter);
+		return false;
+	});
+	
+	$('#menu img').click(function() {
+		if($('#submenu').css('display') == 'block') {
+			$('#submenu').slideUp();
+			$(this).removeClass('ausfahren');
+			$(this).addClass('einfahren');			
+		} else {
+			$('#submenu').slideDown();
+			$(this).removeClass('einfahren');
+			$(this).addClass('ausfahren');			
+		}
+	});
+	
+	$('#menu a').click(function() {
+		return true ;
+	});
+	
+	$('#submenu a').click(function() {
+		return true ;
+	});
+
+	$('#submenu > div').mouseover(function() {
+		$('div.aktiv').removeClass('aktiv');										   
+		$(this).addClass('aktiv');				
+	});
+	
+	$('#menu').mouseleave(function() {
+		$('#submenu').slideUp();
+		$('#menu img').removeClass('ausfahren');
+		$('#menu img').addClass('einfahren');	
+	});
+	
+	/* TEASER */	
+	var intTimeout = 0;
+	var intCurrId  = 0;
+	
+	$('.teaser:eq('+ intCurrId +')').show();
+
+	$("#teaser-navi li").hover( function () {
+		var id = $('#teaser-navi li').index(this);
+		if ($(".teaser:eq("+ id +")").is(":visible") === false) {
+			$('.teaser:eq('+ intCurrId +')').fadeOut();
+			$('.teaser:eq('+ id +')').fadeIn();
+			intCurrId = id;
+			$('#teaser-navi > li').removeClass('aktiv');
+			$('#teaser-navi > li:eq(' + (intCurrId)+ ')').addClass('aktiv');
+			window.clearInterval(intTimeout);			
+		}
+	});
+
+	changeImg = function() {
+		$('.teaser:eq('+ intCurrId +')').fadeOut(2000);
+		intCurrId = ( intCurrId == 2 ) ? 0 : intCurrId + 1;
+		$('.teaser:eq('+ intCurrId +')').fadeIn(2000);
+		$('#teaser-navi > li').removeClass('aktiv');
+		$('#teaser-navi > li:eq(' + (intCurrId)+ ')').addClass('aktiv');
+	}
+
+	intTimeout = window.setInterval("changeImg()", 10000);
+	
+	/* NEWS 
+	$('.newshead').hover(function() {						  
+		var nid = $('.newshead').index(this);
+		
+		if($('.further:eq(' + nid + ')').is(':visible') == false ) {
+			$('.further:visible').hide();
+			$('.aktiv').removeClass('aktiv');
+			$('.further:eq(' + nid + ')').slideDown().addClass('aktiv');
+			$(this).addClass('aktiv');
+		}
+	}); */
+
+	/* TABELLEN */
+	$('#team').tablesorter({ 
+        // sort on the first column and third column, order asc 
+		sortList: [[1, 0],[0, 0]]
+    }); 
+	$('#profil tr:odd').css('background-color','#F1F1F1');
+	$('#team tr:odd').css('background-color','#F1F1F1');
+	$('#coach tr:odd').css('background-color','#F1F1F1');
+	$('#supporter tr:odd').css('background-color','#F1F1F1');	
+
+	/* LIGHTBOX */
+//	$('a[rel*=lightbox]').lightBox();
+	Shadowbox.init();
+
+/*
+	
+	if(($('#newsflash').length > 0) && (document.cookie) ) {
+		lb = document.cookie;
+		if(lb.match(/lb/)){
+		}
+		else {
+
+		var ablauf = new Date();
+		var inEinemTag = ablauf.getTime() + (1 * 24 * 60 * 60 * 1000);
+		ablauf.setTime(inEinemTag);
+		document.cookie = "lb=start; expires=" + ablauf.toGMTString();
+		var overlay = '<div id="overlay" style="filter:alpha(opacity=75); -moz-opacity: 0.75; opacity: 0.75;background:#000; position:fixed; top:0; left:0; height:100em; width:100%; z-index:200000;"></div>';
+		$('body').prepend(overlay); 
+		$('#newsflash').fadeIn(1800);
+
+		}
+	}  */
+
+	$('.close, #overlay').live('click', function(){ 
+			$('#newsflash, #overlay').fadeOut(function(){ 
+				$('#overlay').remove();
+			});
+			return false;
+		});
+	
+
+});
